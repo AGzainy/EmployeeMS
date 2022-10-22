f2f72f68cfb3a2f24c2b20c187fe868651a0b1e2
AGzainy <62115527+AGzainy@users.noreply.github.com>
2022-10-22 23:22:46 +0300

    all

---
+++ src/main/java/com/example/demo/service/EmployeeServiceImpl.java
@@ -0,0 +1,46 @@
+package com.example.demo.service;
+
+import com.example.demo.model.Employee;
+import com.example.demo.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService{
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+    @Override
+    public List<Employee> getAllEmployees() {
+
+        return employeeRepository.findAll();
+    }
+
+    @Override
+    public void saveEmployee(Employee employee) {
+
+        if(!employee.getFirstName().isEmpty() && !employee.getLastName().isEmpty() && !employee.getEmail().isEmpty()) {
+            this.employeeRepository.save(employee);
+        }
+    }
+
+    @Override
+    public Employee getEmployeeById(long id) {
+        Optional<Employee> optional = employeeRepository.findById(id);
+        Employee employee = null;
+        if(optional.isPresent()){
+            employee = optional.get();
+        }else{
+            throw new RuntimeException("Employee not found by id:" + id);
+        }
+        return employee;
+    }
+
+    @Override
+    public void deleteEmployeeById(long id) {
+        this.employeeRepository.deleteById(id);
+    }
+}
